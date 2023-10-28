596b74dd182238d4db056c6256086dc5dbcb888d
enfomatkreyol <122720971+enfomatkreyol@users.noreply.github.com>
2023-10-29 00:38:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function voireplusf(){
+    document.getElementById('detailsform').style.display='block';
+    document.getElementById('box1').style.display='none';
+   }
+  
+   function voireplusb(){
+    document.getElementById('detailsb').style.display='block';
+    document.getElementById('box2').style.display='none';
+   }
+  
+   function voireplusformation(){
+    document.getElementById('detailsformation').style.display='block';
+    document.getElementById('box3').style.display='none';
+   }
